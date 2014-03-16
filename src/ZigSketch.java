ce79da74d2fa21affe637db4635f114a622275c7
alistaircolling <alistair.colling@gmail.com>
2014-03-16 12:38:50 +0000

    new class

---
+++ src/ZigSketch.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import toxi.geom.Vec2D;
+
+import com.hookedup.processing.ExtraWindow;
+
+public class ZigSketch extends ExtraWindow {
+
+	public ZigSketch(PApplet theApplet, String theName, int theWidth,
+			int theHeight) {
+		super(theApplet, theName, theWidth, theHeight);
+		
+	}
+
+	public void setup() {
+		size(350, 300);
+
+	}
+
+	
+
+	public void draw() {
+
+		
+	}
+
+	
+}
+
